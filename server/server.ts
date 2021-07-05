b55ed16c2c5aa8b10d40960b738f2c019607030c
Shivaraj <shivaraja.hm@spurtreetech.in>
2021-07-05 14:45:39 +0530

    Swagger integration done with existing model

--- server/server.ts
+++ server/server.ts
@@ -1,30 +1,9 @@
-// import express from 'express';
-// import bodyParser from "body-parser";
-// // import routes from './routes'
-// const app = express();
-// // var router = express.Router()
-
-// app.use(bodyParser.json());
-
-// // router(routes)
-
-// app.get("/", (req, res, next) => {
-  
-//  res.json({ message: "from index api" });
-
-// });
-
-
-// app.listen(8080, () => {
-
-//   console.log(`Server is running`);
-
-
-// });
-
 import express, { Application } from "express";
 import morgan from "morgan";
 import Router from "./routes";
+import swaggerUi from 'swagger-ui-express'
+
+const swaggerDocument = require('./swagger/swagger.json');
 
 const PORT = process.env.PORT || 8000;
 
@@ -34,6 +13,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use(Router);
+
+var options = {
+  explorer: true
+};
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
